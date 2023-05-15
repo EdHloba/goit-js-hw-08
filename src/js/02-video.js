91b2018806ff640b09cb81dcaaaa827d1b81f5c1
EdHloba <hloba.e.v@gmail.com>
2023-05-15 17:55:36 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+currentTime();
+
+player.on('timeupdate', throttle(timeUpdateHendler, 1000));
+
+function timeUpdateHendler(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+function currentTime() {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    const data = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+    if (data.seconds === data.duration) {
+      localStorage.removeItem('videoplayer-current-time');
+      return;
+    }
+
+    player.setCurrentTime(data.seconds);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name = email]');
+const messageInput = document.querySelector('textarea[name = message]');
+
+
+let formInputData = {};
+
+setFormData();
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInput(evt) {
+  formInputData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formInputData));
+}
+
+function setFormData() {
+  if (localStorage.getItem('feedback-form-state')) {
+    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    formInputData = { ...data };
+
+    emailInput.value = data.email ? data.email : '';
+    messageInput.value = data.message ? data.message : '';
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const submitedData = { ...JSON.parse(localStorage.getItem('feedback-form-state')) };
+
+  if (Object.keys(submitedData).length === 0) {
+    return alert('Поля вводу повинні бути заповненими!');
+  }
+
+  const isAllInputFilled =
+    submitedData.hasOwnProperty('email') &&
+    submitedData.hasOwnProperty('message');
+
+  if (!isAllInputFilled) {
+    return alert('Усі поля мають бути заповнені!');
+  }
+
+  console.log(submitedData);
+
+  event.currentTarget.reset();
+
+  localStorage.removeItem('feedback-form-state');
+
+}
